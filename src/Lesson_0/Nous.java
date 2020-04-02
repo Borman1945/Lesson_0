cf6bcb863fca3d02f7bb007855e01c7deba08d8d
Буторин Михаил Дмитриевич <mbutorin@phoenixit.ru>
2020-04-03 00:56:35 +0300

    Initial commit

---
+++ src/Lesson_0/Nous.java
@@ -0,0 +1,13 @@
+package Lesson_0;
+
+public class Nous {
+    public static void main(String[] args){
+
+        int[][] arr = {{1,2},{3}};
+
+        System.out.println(arr[0][0] + "  " + arr[1][0] + "  " + arr[0][1]);
+
+    }
+
+
+}
